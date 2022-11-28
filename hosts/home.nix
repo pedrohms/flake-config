f67d11b0262f588e2d40a253b1883ab22e33403f
Pedro Souza <pedro@pedro.com>
2022-11-27 21:45:52 -0300

    Try to fix something

--- hosts/home.nix
+++ hosts/home.nix
@@ -25,7 +25,7 @@
       flutter
     ];
 
-    nixpkgs.overlays = [
+    pkgs.overlays = [
       (import (builtins.fetchTarball {
         url = https://github.com/nix-community/neovim-nightly-overlay/archive/master.tar.gz;
         sha256 = "1iiry87sm1zm351l744xjwz8vg3h9xa3ngll89p2lr42jzmnj5v8";
