2c824f071502fc08ec8b627f998a7937b77db4c1
Pedro Souza <pedrohms412@gmail.com>
2022-12-13 01:08:49 -0300

    Added pkgs to vm host

--- hosts/default.nix
+++ hosts/default.nix
@@ -56,7 +56,7 @@ in
       home-manager.nixosModules.home-manager {
         home-manager.useGlobalPkgs = true;
         home-manager.useUserPackages = true;
-        home-manager.extraSpecialArgs = { inherit user; }; 
+        home-manager.extraSpecialArgs = { inherit user pkgs; }; 
         home-manager.users.${user} = {
           imports = [(import ./home.nix)] ++ [(import ./vm/home.nix)];
         };
