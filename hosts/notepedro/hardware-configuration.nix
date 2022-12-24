c53968970b60ea9477bd189ccb3ce4db32b13109
Pedro Souza <pedrohms412@gmail.com>
2022-12-23 21:13:20 -0300

    Added bridge network on notepedro host

--- hosts/notepedro/hardware-configuration.nix
+++ hosts/notepedro/hardware-configuration.nix
@@ -37,6 +37,14 @@
     hostName = "notepedro";
     useDHCP = lib.mkDefault true;
     networkmanager.enable = true;
+    bridges.br0.interfaces = ["virbr0"];
+    interfaces.br0 = {
+      useDHCP = false;
+      ipv4.addresses = [ {
+        "address" = "192.168.122.1";
+        "prefixLength" = 24;
+      } ];
+    };
     # interfaces = {
     #   enp0s20f0u2u4 = {
     #     useDHCP = true;
