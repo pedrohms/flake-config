c354a013d953a5dd1f49546cedd5ae5cc7c6b066
Pedro Souza <pedrohms412@gmail.com>
2023-01-10 15:25:35 -0300

    Added pointercursor to home-manager on desenv07

--- nix/desenv07/home.nix
+++ nix/desenv07/home.nix
@@ -1,11 +1,20 @@
 { inputs, user, pkgs, my-overlays, ... }:
 
 {
-  home.packages = with pkgs; [ 
-    xarchiver
-    ghq
-    sumneko-lua-language-server
-    authenticator
-    discord
-  ];
+  home = {
+    packages = with pkgs; [ 
+      xarchiver
+      ghq
+      sumneko-lua-language-server
+      authenticator
+      discord
+    ];
+    pointerCursor = {                         # This will set cursor systemwide so applications can not choose their own
+      #name = "Dracula-cursors";
+      name = "Catppuccin-Mocha-Dark-Cursors";
+      #package = pkgs.dracula-theme;
+      package = pkgs.catppuccin-cursors.mochaDark;
+      size = 16;
+    };
+  }
 }
