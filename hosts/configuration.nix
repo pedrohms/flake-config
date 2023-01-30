9955071987f560e3bffd5fe9a8cfd88f3665a0c9
Pedro Souza <pedrohms412@gmail.com>
2023-01-30 09:19:27 -0300

    Fixing rtkit position

--- hosts/configuration.nix
+++ hosts/configuration.nix
@@ -41,21 +41,21 @@
     };
   };
 
+  security.rtkit.enable = true;
   services = {
     udev.packages = [
      pkgs.android-udev-rules
     ];
     gnome.gnome-keyring.enable = true;
     pcscd.enable = true;
-  security.rtkit.enable = true;
-  services.pipewire = {
-    enable = true;
-    alsa.enable = true;
-    alsa.support32Bit = true;
-    pulse.enable = true;
-    # If you want to use JACK applications, uncomment this
-    jack.enable = true;
-  };
+    services.pipewire = {
+      enable = true;
+      alsa.enable = true;
+      alsa.support32Bit = true;
+      pulse.enable = true;
+      # If you want to use JACK applications, uncomment this
+      jack.enable = true;
+    };
   };
 
   users.users.${user} = {
