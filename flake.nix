f22f6e4b0065ca052431a7b17098a7d029561ddf
Pedro Souza <pedrohms412@gmail.com>
2023-02-27 15:25:41 -0300

    Testing getAttres

--- flake.nix
+++ flake.nix
@@ -10,24 +10,28 @@
 
   outputs = inputs @ { self, nixpkgs, home-manager, ... }:
     let
-      user = "pedro"; 
+      # user = "pedro"; 
       # user = "framework"; 
       my-overlays = [
           inputs.neovim-nightly-overlay.overlay
         ];
       location = "$HOME/.setup";
     in {
-      nixosConfigurations = (
+      nixosConfigurations = genAttrs [ "notepedro", "desenv07"]( user: nixosSystem {
+        notepedro = "pedro";
+        desenv07 = "framework";
         import ./hosts {
           inherit (nixpkgs) lib;
           inherit inputs nixpkgs user location  home-manager my-overlays;
         }
-      );
-      homeConfigurations = (
+      });
+      homeConfigurations = genAttrs [ "notepedro", "desenv07"]( user: nixosSystem {
+        notepedro = "pedro";
+        desenv07 = "framework";
         import ./nix {
           inherit (nixpkgs) lib;
           inherit inputs nixpkgs home-manager user my-overlays;
         }
-      );
+      });
     };
 }
