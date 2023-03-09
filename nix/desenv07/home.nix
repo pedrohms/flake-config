78380bdad0f2a6fa7f0083ba23eca6fe43271519
Pedro Souza <pedrohms412@gmail.com>
2023-03-09 14:52:46 -0300

    Fix something

--- nix/desenv07/home.nix
+++ nix/desenv07/home.nix
@@ -10,12 +10,5 @@
       discord
       firefox
     ];
-    pointerCursor = {                         # This will set cursor systemwide so applications can not choose their own
-      #name = "Dracula-cursors";
-      name = "Catppuccin-Mocha-Dark-Cursors";
-      #package = pkgs.dracula-theme;
-      package = pkgs.catppuccin-cursors.mochaDark;
-      size = 16;
-    };
   };
 }
