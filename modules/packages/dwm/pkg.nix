b0ef605f3472d3391cd78a2404e2ac409162ea29
Pedro Souza <pedrohms412@gmail.com>
2023-09-05 17:39:37 -0300

    Updating some dwm configs

--- modules/packages/dwm/pkg.nix
+++ modules/packages/dwm/pkg.nix
@@ -9,8 +9,8 @@ stdenv.mkDerivation rec {
     name  = "6.4.0-050920231255-${myFlakeVersion}";
     owner = "pedrohms";
     repo  = "dwm";
-    rev   = "68da3f38de7c8af6c35ca0b54df0173fc3cfcbb1";
-    sha256 = "BJHoFaSYEKEwm31mamOI2FRDnX7ppI/z0vIjGrfsi8g=";
+    rev   = "2fb13870cdfb4d2d0870d37713278847deb94ffa";
+    sha256 = "fxUUKIo41sXDSyqCxG45mFrRMNezA1d9OwiqYnA/y+I=";
   };
 
   # unpackPhase = ''tar -xf $src'';
