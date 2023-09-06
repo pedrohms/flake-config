ae0228729c476d6046ae65025902e32767ce3f89
Pedro Souza <pedrohms412@gmail.com>
2023-09-06 13:28:20 -0300

    Updated to build dwm

--- flake.nix
+++ flake.nix
@@ -11,7 +11,7 @@
 
   outputs = inputs @ { self, nixpkgs, home-manager, ... }:
     let
-      myFlakeVersion = "1.0.23";
+      myFlakeVersion = "1.0.24";
       user = "pedro"; 
       # user = "framework"; 
       my-overlays = [
--- modules/packages/dwm/pkg.nix
+++ modules/packages/dwm/pkg.nix
@@ -9,7 +9,7 @@ stdenv.mkDerivation rec {
     name  = "6.4.0-050920231255-${myFlakeVersion}";
     owner = "pedrohms";
     repo  = "dwm";
-    rev   = "574d286cc62fc0d0e053626b353382ad306f32f4";
+    rev   = "1b0c0512ef28a4f94a1752674c90eac66d8617d0";
     sha256 = "UBSdQme2PhIFiaiihSb4wxpPRq5RKtarOYZdTaUsQ1E=";
   };
 
