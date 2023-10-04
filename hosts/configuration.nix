240b061362b38096b033a912fb20884271cc67fe
Pedro Souza <pedrohms412@gmail.com>
2023-10-04 18:15:24 -0300

    Added jq and sqlite to all hosts

--- hosts/configuration.nix
+++ hosts/configuration.nix
@@ -106,6 +106,8 @@
     };
     systemPackages = with pkgs; [
       vim # Do not forget to add an editor to edit configuration.nix! The Nano editor is also installed by default.
+      jq
+      sqlite
       wget
       git
       brave
