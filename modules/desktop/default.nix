23ab99ac3f3656f12278dd579a85a544a640df22
Pedro Souza <pedrohms412@gmail.com>
2023-10-09 06:36:00 -0300

    backup

--- modules/desktop/default.nix
+++ modules/desktop/default.nix
@@ -71,6 +71,6 @@
     enable = true;
     wlr.enable = true;
     # gtk portal needed to make gtk apps happy
-    extraPortals = [ pkgs.xdg-desktop-portal-gtk ];
+    # extraPortals = [ pkgs.xdg-desktop-portal-gtk ];
   };
 }
